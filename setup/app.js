a458781ba4603d75f3683a8fa1acc30c8da172b2
Gattem Bharath <53399475+GattemBharath@users.noreply.github.com>
2023-03-18 11:19:37 +0530

    final

--- setup/app.js
+++ setup/app.js
@@ -1,2 +1,31 @@
 //using selectors inside the element
 // traversing the dom
+
+//! Traversing the DOM
+
+/*
+const questions = document.querySelectorAll(".question-btn");
+
+questions.forEach((question) =>
+  question.addEventListener("click", function (e) {
+    const qtn = e.currentTarget.parentElement.parentElement;
+    qtn.classList.toggle("show-text");
+  })
+);
+*/
+
+//! Using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
